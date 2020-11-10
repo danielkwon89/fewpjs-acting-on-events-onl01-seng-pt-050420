bf2ba1dad1c24bdd1ede0c0d19052ef4dfb82de4
danielkwon89 <danielkwon89@gmail.com>
2020-11-09 22:54:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft(){
+    let leftNumber = dodger.style.left.replace("px", "");
+    let number = parseInt(leftNumber, 10);
+    if (number > 0){
+        dodger.style.left = `${number - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    let rightNumber = dodger.style.left.replace("px", "");
+    let number = parseInt(rightNumber, 10);
+    if (number < 400){
+        dodger.style.left = `${number + 1}px`
+    }
+}
